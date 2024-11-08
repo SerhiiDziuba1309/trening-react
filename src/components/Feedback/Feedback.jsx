76da948d00f3275fdef595b375a4fb2799738cb7
SerhiiDziuba1309 <168010375+SerhiiDziuba1309@users.noreply.github.com>
2024-11-08 21:24:08 +0100

    Initial commit

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Feedback = ({ feedback, totalFeedback, positivePercentage }) => {
+  return (
+    <div>
+      <h2>Feedback Statistics</h2>
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total feedback: {totalFeedback}</p>
+      <p>Positive feedback: {positivePercentage}%</p>
+    </div>
+  );
+};
+
+export default Feedback;
